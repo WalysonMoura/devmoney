a4eb473d6e12fd38ba894dc085d416d4d40571ea
{ Walyson } <walysonmoura222@gmail.com>
2024-02-19 13:09:26 +0000

    new

---
+++ src/database.ts
@@ -0,0 +1,15 @@
+import knex, { Knex } from "knex";
+import { env } from "./env";
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? { filename: env.DATABASE_URL }
+      : env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
